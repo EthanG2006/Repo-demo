45aab67a222f5931564f0344235ced42c2a58044
RyanWang12 <ryanwcontacts@gmail.com>
2024-10-04 14:40:24 -0400

    Update main.cpp
    updated so it has reverse iterator

--- HackRPI-demo/HackRPI-demo/main.cpp
+++ HackRPI-demo/HackRPI-demo/main.cpp
@@ -6,11 +6,24 @@
 //
 
 #include <iostream>
+#include <ofstream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
 
-    std::cout<<"yoyoyoy"<<std::endl;
+    std::list<int> numArr;
+
+    //iterate through list
+    for(std::list<int>::iterator itr = numArr.begin(); itr != numArr.end(); itr++){
+
+        std::cout<<*itr<<", ";
+    }
+    
+    //reverse iterator
+    for(std::list<int>::iterator itr = numArr.end(); itr != numArr.begin(); --itr){
+        std::cout<<*itr<<", ";
+    }
+    
+
+    
 }
